e2abff87e25447c82b8ad03926b9803fd1853aa5
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-02-25 22:13:22 +0600

    Add dynamic url and display data

--- meal.js
+++ meal.js
@@ -13,19 +13,42 @@ const searchFood = () => {
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result')
     meals.forEach(meal => {
-        console.log(meal)
+        // console.log(meal)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div onclick = "mealDetails(${meal.idMeal})" class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 200)}...</p>
                 </div>
         </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+const mealDetails = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+
+}
+
+const displayMealDetails = meal => {
+    const singleMealDetails = document.getElementById('single-meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 200)}...</p>
+        <a target="_blank" href="${meal.strYoutube}" class="btn btn-primary">Go YouTube</a>
+    </div>
+    `;
+    singleMealDetails.appendChild(div);
+    console.log(meal)
 }
